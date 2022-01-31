2194a704e0d50e8ac4bb329be9a33aaa2982131c
Yury Avdeev <avdeiev@gmail.com>
2022-01-31 08:57:28 +0300

    094a4a5

---
+++ _nuxt/static/1643608574/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fbank-requests\u002F_nuxt\u002Fstatic\u002F1643608574",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002F",config:{_app:{basePath:"\u002Fbank-requests\u002F",assetsPath:"\u002Fbank-requests\u002F_nuxt\u002F",cdnURL:a}}}}(null));
